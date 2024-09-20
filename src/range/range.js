c27f9d0f36df59e8f72071df339fdc72ba22d68b
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2024-09-20 23:00:20 +0700

    Update range.js

--- src/range/range.js
+++ src/range/range.js
@@ -4,6 +4,16 @@ class Range {
     this.start = start;
     this.stop = stop;
   }
+  static of(start, stop) {
+    this.style = "of";
+    this.start = start;
+    this.stop = stop;
+  }
+  static open(start, stop) {
+    this.style = "open";
+    this.start = start;
+    this.stop = stop;
+  }
 }
 
 module.exports = Range;
