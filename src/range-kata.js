000d2f95264b259f8c258cc9093870541b9eee42
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2024-09-20 23:04:51 +0700

    Update range-kata.js

--- src/range-kata.js
+++ src/range-kata.js
@@ -1,21 +1,21 @@
 const {
-  Of,
-  Open,
-  Closed,
-  ClosedOpen,
-  OpenClosed,
+  of,
+  open,
+  closed,
+  closedOpen,
+  openClosed,
 } = require('./range');
 
 class RangeKata {
   constructor(ranges = []) {
     this.ranges = ranges.map((range) => {
       switch (range.style) {
-        case 'Of': return new Of(range.start, range.stop);
-        case 'Open': return new Open(range.start, range.stop);
-        case 'Closed': return new Closed(range.start, range.stop);
-        case 'ClosedOpen': return new ClosedOpen(range.start, range.stop);
-        case 'OpenClosed': return new OpenClosed(range.start, range.stop);
-        default: return new Of(range.start, range.stop);
+        case 'if': return new of(range.start, range.stop);
+        case 'open': return new open(range.start, range.stop);
+        case 'closed': return new closed(range.start, range.stop);
+        case 'closedOpen': return new closedOpen(range.start, range.stop);
+        case 'openClosed': return new openClosed(range.start, range.stop);
+        default: return new of(range.start, range.stop);
       }
     });
   }
