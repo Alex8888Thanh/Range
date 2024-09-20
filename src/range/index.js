77c112a8ecf9dc5abb483a692fdcd2132af723ef
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2024-09-20 20:54:43 +0700

    Update index.js

--- src/range/index.js
+++ src/range/index.js
@@ -1,1 +1,15 @@
+const Of = require('./of');
+const Open = require('./open');
+const Closed = require('./closed');
+const ClosedOpen = require('./closed-open');
+const OpenClosed = require('./open-closed');
+const RandomRange = require('./random-range');
 
+module.exports = {
+  Of,
+  Open,
+  Closed,
+  ClosedOpen,
+  OpenClosed,
+  RandomRange,
+};
