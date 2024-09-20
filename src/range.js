4527128df14cb6054b5060932bd8dc0d931e9062
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2024-09-20 20:37:56 +0700

    Create range.js

---
+++ src/range.js
@@ -0,0 +1,31 @@
+const {
+  Of,
+  Open,
+  Closed,
+  ClosedOpen,
+  OpenClosed,
+} = require('./range');
+
+class Range {
+  constructor(ranges = []) {
+    this.ranges = ranges.map((range) => {
+      switch (range.style) {
+        case 'Of': return new Of();
+        case 'Open': return new Open(range.start, range.stop);
+        case 'Closed': return new Closed(range.start, range.stop);
+        case 'ClosedOpen': return new ClosedOpen(range.start, range.stop);
+        case 'OpenClosed': return new OpenClosed(range.start, range.stop);
+        default: return new RandomRange(range.start, range.stop);
+      }
+    });
+  }
+
+  updateQuality() {
+    this.ranges.forEach((range) => range.update());
+    return this.ranges;
+  }
+}
+
+module.exports = {
+  Range,
+};
