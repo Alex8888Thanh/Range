df369e6e4d6d6eb29c2aca8a6041049f0bdefca4
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2024-09-20 21:16:01 +0700

    Add files via upload

---
+++ src/range/open-closed.js
@@ -0,0 +1,19 @@
+const Range = require('./range');
+
+class Of extends Range {
+  constructor(start, stop) {
+    super('Of', start, stop);
+  }
+
+  contains(n) {
+    if(this.start<n&&n<this.stop){
+      console.log(true);
+      return true;
+    } else {
+      console.log(false);
+      return false;
+    }
+  }
+}
+
+module.exports = Of;
