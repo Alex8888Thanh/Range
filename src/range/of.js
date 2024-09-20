c5b1b5ebb63fb9a4e6ea818c83b3353946ac75fc
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2024-09-20 21:03:09 +0700

    Update of.js

--- src/range/of.js
+++ src/range/of.js
@@ -1,1 +1,14 @@
+const Range = require('./range');
 
+class Of extends Range {
+  constructor(start, stop) {
+    super('Of', start, stop);
+  }
+
+  update() {
+    this.start = ;
+    this.stop = ;
+  }
+}
+
+module.exports = AgedBrie;
