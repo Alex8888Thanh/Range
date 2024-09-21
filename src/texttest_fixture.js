b083b6be9fef6e05ad87a39f00826692d6ae6cf7
Alex8888Thanh <114840033+Alex8888Thanh@users.noreply.github.com>
2024-09-21 07:58:23 +0700

    Update texttest_fixture.js

--- src/texttest_fixture.js
+++ src/texttest_fixture.js
@@ -3,11 +3,15 @@ const RangeKata = require("./range-kata");
 
 const ranges = [
   new Range("of", 11, 23),
-  Range.of(17, 31),
   new Range("open", 11, 23),
   new Range("closed", 11, 23),
   new Range("closedOpen", 11, 23),
   new Range("openClosed", 11, 23),
+  Range.of(17, 31),
+  Range.open(17, 31),
+  Range.closed(17, 31),
+  Range.closedOpen(17, 31),
+  Range.openClosed(17, 31),
 ];
 
 const nums = Number(process.argv[2]) || 31;
